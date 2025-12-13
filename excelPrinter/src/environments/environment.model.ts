d5a86052124aabac25d483cc7f7249cfbf7fe88e
EBRAHEM-SHAMS <EBRAHEM-SHAMS@DESKTOP-29JJB1R>
2025-12-13 15:50:32 +0200

    lastupdate

--- excelPrinter/src/environments/environment.model.ts
+++ excelPrinter/src/environments/environment.model.ts
@@ -6,32 +6,32 @@ export const environment = {
   qrMessageEn :"Please do not remove",
 
 
-  colorSheetWidthInmm : '1200mm',// عرض البيدج كلها 
+  colorSheetWidthInmm : '1200mm',// عرض البيدج كلها
   colorSheetHeightInmm : '1000mm', ///  ارتفاع البيدج  كلها
-  colorOneTemplateWidthInmm : '90mm',// المسافة من الكورنرز
-  colorOneInnerTemplateWidthInmm : '80mm',/// المسافة من جوا المربع 
+  colorOneTemplateWidthInmm : '129mm',// المسافة من الكورنرز
+  colorOneInnerTemplateWidthInmm : '116mm',/// المسافة من جوا المربع
   colorSheetCountInPage : 143,// عدد الملصقات في الصفحة
-  colorQrCodeScale :8, // حجم الكيو ار
-  colorlogoWidth: 280 ,// عرض لوجو الشركة
-  colorlogoHeight: 280 ,// ارتفاع لوجو الشركة
-  colorlogomarginleft:'-99px', // margin left for logo
+  colorQrCodeScale :9, // حجم الكيو ار
+  colorlogoWidth: 350 ,// عرض لوجو الشركة
+  colorlogoHeight: 350 ,// ارتفاع لوجو الشركة
+  colorlogomarginleft:'-126px', // margin left for logo
   colorlogomarginbottom:'-72px', // margin bottom for logo
-  colorqrCodeTextFontSize :25 , // xxx-yyy-xxxx-00001
-  colorqEnglishTextFontSize :20 , // Please do not remove
-  colorArabicTextFontSize :20 , // من فضلك لا تعبث بهذا الملصق
-  blackWhiteSheetWidthInmm : '297mm', // عرض البيدج كلها 
-  blackWhiteSheetHeightInmm : '420mm', ///  ارتفاع البيدج  كلها
+  colorqrCodeTextFontSize :34 , // xxx-yyy-xxxx-00001
+  colorqEnglishTextFontSize :34 , // Please do not remove
+  colorArabicTextFontSize :34 , // من فضلك لا تعبث بهذا الملصق
+  blackWhiteSheetWidthInmm : '630mm', // عرض البيدج كلها
+  blackWhiteSheetHeightInmm : '630mm', ///  ارتفاع البيدج  كلها
   blackWhiteOneTemplateWidthInmm : '110mm', // المسافة من الكورنرز
-  blackWhiteOneInnerTemplateWidthInmm : '95mm', /// المسافة من جوا المربع 
-  blackWhiteSheetCountInPage : 6,// عدد الملصقات في الصفحة
-  blackWhiteQrCodeScale :9,// حجم الكيو ار 
-  blackWhitelogoWidth: 270 , // عرض لوجو الشركة 
+  blackWhiteOneInnerTemplateWidthInmm : '95mm', /// المسافة من جوا المربع
+  blackWhiteSheetCountInPage : 20,// عدد الملصقات في الصفحة
+  blackWhiteQrCodeScale :8,// حجم الكيو ار
+  blackWhitelogoWidth: 280 , // عرض لوجو الشركة
   blackWhitelogoHeight: 280 , // ارتفاع لوجو الشركة
-   blackWhitelogomarginleft:'-86px', // margin left for logo
+   blackWhitelogomarginleft:'-91px', // margin left for logo
   blackWhitelogomarginbottom:'-37px',// margin bottom for logo
   blackWhiteqrCodeTextFontSize :30 ,// xxx-yyy-xxxx-00001
-  blackWhiteqEnglishTextFontSize :25 ,// Please do not remove
-  blackWhiteArabicTextFontSize :25 ,// من فضلك لا تعبث بهذا الملصق
+  blackWhiteqEnglishTextFontSize :30 ,// Please do not remove
+  blackWhiteArabicTextFontSize :30 ,// من فضلك لا تعبث بهذا الملصق
   blackTextImageWidthPercentage : '80%',//Please do not remove // من فضلك لا تعبث بهذا الملصق
 
 
